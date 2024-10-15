dfb776d804353dc552c0465d3bf7317fa456cf93
7tono <shufalcono@gmail.com>
2024-10-15 17:43:40 +0900

    MouseMoveが動かない　Tickで空白を上のだんごで埋める

--- Dangopzl/Form1.cs
+++ Dangopzl/Form1.cs
@@ -8,31 +8,17 @@ namespace Dangopzl
     {
         Graphics g;
 
-
+        int selx = -50;
+        int sely = -50;
 
         public Form1()
         {
             InitializeComponent();
             Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            g = Graphics.FromImage(canvas);
+            //g = Graphics.FromImage(canvas);
 
-            //ボーダー線(縦)
-            for (int r = 0; r <= 357; r += 51)
-            {
-                g.DrawLine(Pens.Black, r, 0, r, 511);
-                
-            }
-
-
-            //ボーダー線(横) 357
-            for (int r = 0; r <= 510; r += 51)
-            {
-                g.DrawLine(Pens.Black, 0, r, 306, r);
-                
-            }
-
-
-            g.Dispose();
+            g = pictureBox1.CreateGraphics();
+            // g.Dispose();
             pictureBox1.Image = canvas;
 
         }
@@ -77,27 +63,28 @@ namespace Dangopzl
         int scorecounter = 0;
 
         //マップ作成
-        int[,] intValues = new int[10, 6]
+        int[,] intValues = new int[11, 7]
             {
-                {0, 1, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 0, 0},
-                {0, 0, 0, 0, 1, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0,0},
+                {0, 0, 0, 0, 0, 0,0},
+                {0, 0, 0, 0, 0, 0,0},
+                {0, 0, 0, 0, 0, 0,0},
+                {0, 1, 2, 3, 4, 0,0},
+                {0, 0, 0, 0, 0, 0,0},
+                {0, 0, 0, 0, 0, 0,0},
+                {0, 0, 0, 0, 0, 0,0},
+                {0, 0, 0, 0, 0, 0,0},
+                {0, 0, 0, 0, 0, 0,0},
+                {0, 0, 0, 0, 0, 0,0},
             };
         private void makemap()　//マップ初期化
         {
-            
+
             for (int r = 0; r <= 9; r++)//行
             {
                 for (int c = 0; c <= 5; c++)//列
                 {
-                    if (intValues[r, c] == 1)
+                    if (intValues[r, c] != 0)
                     {
                         intValues[r, c] = 0;
                     }
@@ -123,7 +110,7 @@ namespace Dangopzl
                 listView1.Items.Add(new ListViewItem(new string[] { scorecounter_str, nuwscore }));
             }
 
-            for(int r = 0; r <= 9; r++)//行
+            for (int r = 0; r <= 9; r++)//行
             {
                 for (int c = 0; c <= 5; c++)//列
                 {
@@ -133,23 +120,82 @@ namespace Dangopzl
                     }
                 }
             }
+
+            makemap();
+            mapshafull();
         }
 
-       
+
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
+
+
+
+
+            e.Graphics.FillRectangle(Brushes.Silver, 0, 0, Width, Height);
+
+            //ボーダー線(縦)
+            for (int r = 0; r <= 357; r += 51)
+            {
+                e.Graphics.DrawLine(Pens.Black, r, 0, r, 511);
+
+            }
+            //ボーダー線(横) 357
+            for (int r = 0; r <= 510; r += 51)
+            {
+                e.Graphics.DrawLine(Pens.Black, 0, r, 306, r);
+
+            }
+
+
+
+
+
+
             for (int r = 0; r <= 9; r++)//行
             {
                 for (int c = 0; c <= 5; c++)//列
                 {
+
+                    //だんごを出現させる
+
                     if (intValues[r, c] == 1)
                     {
-                        //g.DrawLine(Pens.Red,1, 1, 1, 1);
-                        e.Graphics.FillEllipse(Brushes.Red, c*51+2, r*51+2, 48, 48);
+                        //g.DrawLine(Pens.Red, 1, 1, 1, 1);
+                        e.Graphics.FillEllipse(Brushes.Gray, c * 51 + 2, r * 51 + 2, 48, 48);
+
                     }
+                    if (intValues[r, c] == 2)
+                    {
+                        //g.DrawLine(Pens.Red,1, 1, 1, 1);
+                        e.Graphics.FillEllipse(Brushes.Yellow, c * 51 + 2, r * 51 + 2, 48, 48);
+
+                    }
+                    if (intValues[r, c] == 3)
+                    {
+                        //g.DrawLine(Pens.Red,1, 1, 1, 1);
+                        e.Graphics.FillEllipse(Brushes.Brown, c * 51 + 2, r * 51 + 2, 48, 48);
+
+                    }
+                    if (intValues[r, c] == 4)
+                    {
+                        //g.DrawLine(Pens.Red,1, 1, 1, 1);
+                        e.Graphics.FillEllipse(Brushes.Green, c * 51 + 2, r * 51 + 2, 48, 48);
+
+                    }
+
                 }
             }
+
+
+
+
+            if (intValues[sely / 51, selx / 51] != 0)
+            {
+                e.Graphics.DrawEllipse(Pens.Red, selx, sely, 49, 49);
+            }
+
         }
 
 
@@ -159,21 +205,55 @@ namespace Dangopzl
         int value2;
         int value3;
 
+        private void mapshafull()
+        {
+            var rand_num = new Random();
+
+            for (int r = 0; r <= 9; r++)//行
+            {
+                for (int c = 0; c <= 5; c++)//列
+                {
+                    value3 = rand_num.Next(minValue: 1, maxValue: 5);  //だんごの種類数（上のプログラムで出現させる）
+                    intValues[r, c] = value3;
+                }
+            }
+
+
+
+            value3 = rand_num.Next(minValue: 1, maxValue: 5);  //だんごの種類数（上のプログラムで出現させる）
+            intValues[value1, value2] = value3;
+            pictureBox1.Refresh();
+        }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            /*
             var rand_row = new Random();
-            value1 = rand_row.Next(minValue: 0, maxValue:10);
+            value1 = rand_row.Next(minValue: 0, maxValue: 10);
             var rand_colm = new Random();
-            value2 = rand_row.Next(minValue: 0, maxValue: 6);
+            value2 = rand_colm.Next(minValue: 0, maxValue: 6);
             var rand_num = new Random();
-            value3 = rand_row.Next(minValue: 0, maxValue: 2);
+            value3 = rand_num.Next(minValue: 1, maxValue: 5);  //だんごの種類数（上のプログラムで出現させる）
 
 
-            intValues[value1, value2] = value3;
+            //intValues[value1, value2] = value3;
 
 
             pictureBox1.Refresh();
+            */
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //MouseMoveが動かない　Tickで空白を上のだんごで埋める　10/15
+            selx = (e.X / 51) * 51 + 2;
+            sely = (e.Y / 51) * 51 + 2;
+
         }
     }
 }
